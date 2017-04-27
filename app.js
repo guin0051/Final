d35d9d38bc488eabe4867d2deb11e2d26ba05b08
guin0051 <guin0051@algonquinlive.com>
2017-04-27 10:27:17 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+var todoApp = angular.module('TodoApp', ['firebase']);
+
+
+todoApp.controller('TodoCtrl', ['$scope', '$firebaseArray',
+    function ($scope, $firebaseArray) {
+
+        // CREATE A FIREBASE REFERENCE
+        var todosRef = new Firebase('https://todoapp-323f3.firebaseio.com/');
+
+        // GET TODOS AS AN ARRAY
+        $scope.todos = $firebaseArray(todosRef);
+
+        /* Task list and functions */
+
+        $scope.todoDate = null;
+
+        // ADD TODO ITEM METHOD
+        $scope.addTodo = function () {
+
+            // CREATE A UNIQUE ID
+            var timestamp = new Date().valueOf();
+
+            $scope.todos.$add({
+                id: timestamp,
+                name: $scope.todoName,
+                comment: $scope.todoComment,
+                category: $scope.todoCategory,
+                priority: $scope.todoPriority,
+                date: $scope.todoDate.toString(),
+                status: 'pending'
+            });
+
+            $scope.todoName = "";
+            $scope.todoComment = "";
+            $scope.todoCategory = "";
+        };
+
+        // SAVE TODO
+        $scope.saveTodo = function (todo) {
+            $scope.todos.$save(todo);
+        };
+
+
+        // REMOVE TODO ITEM METHOD
+        $scope.removeTodo = function (index, todo) {
+
+            // CHECK THAT ITEM IS VALID
+            if (todo.id === undefined) return;
+
+            // FIREBASE: REMOVE ITEM FROM LIST
+            $scope.todos.$remove(todo);
+
+        };
+
+        // MARK TODO AS IN PROGRESS METHOD
+        $scope.startTodo = function (index, todo) {
+
+            // CHECK THAT ITEM IS VALID
+            if (todo.id === undefined) return;
+
+            // UPDATE STATUS TO IN PROGRESS AND SAVE
+            todo.status = 'in progress';
+            $scope.todos.$save(todo);
+
+        };
+
+        // MARK TODO AS COMPLETE METHOD
+        $scope.completeTodo = function (index, todo) {
+
+            // CHECK THAT ITEM IS VALID
+            if (todo.id === undefined) return;
+
+            // UPDATE STATUS TO COMPLETE AND SAVE
+            todo.status = 'complete';
+            $scope.todos.$save(todo);
+        };
+
+}]);
